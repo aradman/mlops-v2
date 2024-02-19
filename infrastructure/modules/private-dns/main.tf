4ed92dd7dd0b42474967fa684a39e81dd992f3d3
aradman <alirezaradman@yahoo.com>
2024-02-19 23:09:05 +1100

    update repo

---
+++ infrastructure/modules/private-dns/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_private_dns_zone" "private_dns_zone_ml_api" {
+  name                = "privatelink.api.azureml.ms"
+  resource_group_name = var.rg_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "database" {
+  name                  = "mlops-vnet-api"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_ml_api.name
+  virtual_network_id    = module.vnet.output.id
+}
