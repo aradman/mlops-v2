29d58193955a47ddf1135b90eb61e7d4cd981e72
aradman <alirezaradman@yahoo.com>
2024-02-19 20:19:56 +1100

    update repo

--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.prefix}-${var.postfix}${var.env}"
   location            = var.location
   resource_group_name = var.rg_name
-  address_space       = var.vnet_cidr
+  address_space       = [var.vnet_cidr]
   tags = var.tags
     
   lifecycle {
