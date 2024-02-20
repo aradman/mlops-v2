15a5c3780295ebf88649e276bfbe85cfd62c8503
aradman <alirezaradman@yahoo.com>
2024-02-20 14:09:11 +1100

    update repo

--- infrastructure/aml_deploy.tf
+++ infrastructure/aml_deploy.tf
@@ -102,46 +102,43 @@ module "container_registry" {
   tags = local.tags
 }
 
+# Azure Machine Learning workspace
 
-# # Azure Machine Learning workspace
+module "aml_workspace" {
+  source = "./modules/aml-workspace"
 
-# module "aml_workspace" {
-#   source = "./modules/aml-workspace"
+  rg_name  = module.resource_group.name
+  location = module.resource_group.location
 
-#   rg_name  = module.resource_group.name
-#   location = module.resource_group.location
+  prefix  = var.prefix
+  postfix = var.postfix
+  env = var.environment
 
-#   prefix  = var.prefix
-#   postfix = var.postfix
-#   env = var.environment
+  storage_account_id      = module.storage_account_aml.id
+  key_vault_id            = module.key_vault.id
+  application_insights_id = module.application_insights.id
+  container_registry_id   = module.container_registry.id
 
-#   storage_account_id      = module.storage_account_aml.id
-#   key_vault_id            = module.key_vault.id
-#   application_insights_id = module.application_insights.id
-#   container_registry_id   = module.container_registry.id
+  enable_aml_computecluster = var.enable_aml_computecluster
+  storage_account_name      = module.storage_account_aml.name
 
-#   enable_aml_computecluster = var.enable_aml_computecluster
-#   storage_account_name      = module.storage_account_aml.name
+  tags = local.tags
+}
 
-#   tags = local.tags
-# }
+# Application insights
 
+module "application_insights" {
+  source = "./modules/application-insights"
 
+  rg_name  = module.resource_group.name
+  location = module.resource_group.location
 
-# # Application insights
+  prefix  = var.prefix
+  postfix = var.postfix
+  env = var.environment
 
-# module "application_insights" {
-#   source = "./modules/application-insights"
-
-#   rg_name  = module.resource_group.name
-#   location = module.resource_group.location
-
-#   prefix  = var.prefix
-#   postfix = var.postfix
-#   env = var.environment
-
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 # module "data_explorer" {
 #   source = "./modules/data-explorer"
