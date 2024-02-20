6c121052e799731b455c2d9d9ff0f431b9956a61
aradman <alirezaradman@yahoo.com>
2024-02-21 09:17:17 +1100

    update repo

--- infrastructure/modules/pe-nsg/variables.tf
+++ infrastructure/modules/pe-nsg/variables.tf
@@ -7,7 +7,11 @@ variable "location" {
   type        = string
   description = "Location of the resource group"
 }
-
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "A mapping of tags which should be assigned to the Resource Group"
+}
 variable "pe_subnet_id" {
   type        = string
   description = "Private endpoint subnet id"
