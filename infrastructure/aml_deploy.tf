bf65dfbabbd92cd36c7ea42e02d4c8de10ecf22c
aradman <alirezaradman@yahoo.com>
2024-02-21 15:21:44 +1100

    update repo

--- infrastructure/aml_deploy.tf
+++ infrastructure/aml_deploy.tf
@@ -34,6 +34,7 @@ module "subnet" {
   pe_subnet_cidr = "10.0.1.0/24"
   training_subnet_cidr = "10.0.2.0/24"
   scoring_subnet_cidr = "10.0.3.0/24"
+  management_subnet_cidr = "10.0.4.0/24"
 }
 
 # Private endpoint network security group
